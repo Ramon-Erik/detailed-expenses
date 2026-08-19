220dda2174e15aaf75615e406b3574e42f08241e
Ramon Silva <ramonerik29a@gmail,com>
2026-08-19 15:32:41 -0300

    feat: login form

--- src/modules/login/ui/views/LoginView.vue
+++ src/modules/login/ui/views/LoginView.vue
@@ -2,15 +2,32 @@
   <v-container>
     <v-row>
       <v-col>
-        <div class="d-flex flex-column aling-center justify-center text-center" style="min-height: 90vh">
-          <h2 class="text-center">detalhaaí</h2>
-          <p>Sua gestão financeira em detalhes.</p>
+        <div class="d-flex flex-column aling-center justify-center" style="min-height: 90vh">
+          <div class="text-center">
+            <h2 class="text-center my-1">detalha<span class="text-title">aí</span></h2>
+            <p class="mt-1">Sua gestão financeira em detalhes.</p>
+          </div>
           <form-container>
-              <v-text-field label="Email" />
-              <v-text-field label="Senha" type="password" />
-              <v-row>
-                <v-btn text="realizar login" class="w-100 py-6" />
-              </v-row>
+            <v-row>
+              <v-col>
+                <span class="d-block my-3">Email</span>
+                <v-text-field variant="solo-filled" label="Seu melhor email" hide-details="auto" />
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <span class="d-block my-3">Senha</span>
+                <v-text-field
+                  variant="solo-filled"
+                  type="password"
+                  label="Uma senha forte"
+                  hide-details="auto"
+                />
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-btn text="realizar login" class="w-100 py-6" color="primary text-uppercase" />
+            </v-row>
           </form-container>
         </div>
       </v-col>
