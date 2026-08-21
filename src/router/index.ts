e8ca29dd2e477618fe0ccba889600175e2730796
Ramon Silva <ramonerik29a@gmail,com>
2026-08-21 14:59:00 -0300

    feat: routes for login and signin

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@
 
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/modules/login/ui/views/LoginView.vue'
+import SigninView from '@/modules/login/ui/views/SigninView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +18,10 @@ const router = createRouter({
     {
       path: '/login',
       component: LoginView,
+    },
+    {
+      path: '/signin',
+      component: SigninView,
     },
   ],
 })
