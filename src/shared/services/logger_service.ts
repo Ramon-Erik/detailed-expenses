7abde23a3745afc7825de7902e8dcff0569d224a
Ramon Silva <ramonerik29a@gmail,com>
2026-08-21 14:57:28 -0300

    feat: creating logger service

---
+++ src/shared/services/logger_service.ts
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+
+import axios from "axios";
+
+export default class LoggerService {
+  private isDev: boolean
+
+  constructor(environment = import.meta.env.VITE_API_BASE_URL) {
+    this.isDev = environment === 'development'
+  }
+
+  axiosError(message: string, error: unknown) {
+    if (axios.isAxiosError(error) && error.response) {
+      const errorResponse = error.response.data as { message: string};
+      console.error(message, errorResponse.message);
+    }
+  }
+
+  debug(message: string, data?: unknown) {
+    if (this.isDev) console.log(message, data)
+  }
+
+  error(message: string, error?: unknown) {
+    if (this.isDev) console.error(message, error)
+  }
+}
+
+export const loggerService = new LoggerService()
