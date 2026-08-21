c2147a42944b38c4915127421289dd97001d1489
Ramon Silva <ramonerik29a@gmail,com>
2026-08-21 16:07:04 -0300

    feat: remove prepare request from base repository

--- src/shared/repositories/base_repository.ts
+++ src/shared/repositories/base_repository.ts
@@ -5,44 +5,8 @@ import { loggerService } from '../services/logger_service'
 export default class BaseRepository {
   private readonly baseUrl = axiosInstance.defaults.baseURL
 
-  private isRefreshable() {
-    const actualDateTime = sessionStorage.getItem('refresh_token_expiration')
-    if (!actualDateTime) return
-    const targetDate = new Date(actualDateTime)
-
-    const givenDateTime = new Date()
-    const expirationDateTime = new Date(targetDate.getTime() + 7 * 60000)
-
-    if (givenDateTime === expirationDateTime) true
-    return false
-  }
-
-  private async refreshAccessToken(token: string) {
-    const response = await axiosInstance.post('/auth/refresh', { refreshToken: token })
-
-    return response.data.data as RefreshResponse
-  }
-
-  private async prepareRequest() {
-    const refreshToken = sessionStorage.getItem('refresh_token')
-    if (!refreshToken) return
-
-    if (this.isRefreshable()) {
-      const response = await this.refreshAccessToken(refreshToken)
-      const accessToken = response.access_token
-      const newRefreshToken = response.refresh_token
-      const accessTokenExpirationTime = response.expires_at.toString()
-
-      sessionStorage.setItem('access_token', accessToken)
-      sessionStorage.setItem('refresh_token', newRefreshToken)
-      sessionStorage.setItem('refresh_token_expiration', accessTokenExpirationTime)
-    }
-  }
-
   protected async get<T>(path: string, params: object) {
     try {
-      await this.prepareRequest();
-
       const url = this.baseUrl + path
       const response = axiosInstance.get<T>(url, params)
       return (await response).data
@@ -51,12 +15,10 @@ export default class BaseRepository {
     }
   }
 
-  protected async post<T>(path: string, params: object) {
+  protected async post<T, K>(path: string, params: K) {
     try {
-      await this.prepareRequest();
-
       const url = this.baseUrl + path
-      const response = axiosInstance.get<T>(url, params)
+      const response = axiosInstance.post<T>(url, params)
       return (await response).data
     } catch (error) {
       loggerService.axiosError('GET', error)
