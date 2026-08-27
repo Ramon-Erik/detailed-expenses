d143cb405030b229b492a0b58c9571f9f2ecf0b4
Ramon Silva <ramonerik29a@gmail,com>
2026-08-27 12:03:00 -0300

    chore: no unused

--- eslint.config.js
+++ eslint.config.js
@@ -4,5 +4,19 @@ export default vuetify({
   ts: true,
   rules: {
     '@stylistic/space-before-function-paren': 'off',
+    '@unused-imports/no-unused-imports': 'error',
+
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-unused-vars': 'off',
+
+    '@unused-imports/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
   },
 })
