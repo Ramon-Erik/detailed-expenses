ce2c4f601fd44d0a7999f938a6bf61f0996945e0
Ramon Silva <ramonerik29a@gmail,com>
2026-09-01 13:05:07 -0300

    feat: header font size

--- src/shared/layout/HeaderLayout.vue
+++ src/shared/layout/HeaderLayout.vue
@@ -1,5 +1,5 @@
 <template>
   <header class="pa-3">
-    <p class="my-0 font-weight-bold text-center">Detalha<span class="text-primary">ai</span></p>
+    <p class="my-0 font-weight-bold text-center text-headline-small">Detalha<span class="text-primary">ai</span></p>
   </header>
 </template>
